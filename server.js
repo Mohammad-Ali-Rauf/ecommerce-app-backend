4f41c2ecf72b632fbc13f9ff818b2e198fab1a50
Mohammad-Ali-Rauf <85075073+Mohammad-Ali-Rauf@users.noreply.github.com>
2022-02-18 21:18:35 +0500

    -
    -

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+
+app.use(express.json({ extended : false }));
+
+connectDB();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "This is a ecommerce app" })
+});
+
+app.use("/api/auth", require('./routes/auth'));
+app.use("/api/users", require('./routes/users'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

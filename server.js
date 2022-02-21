d33ef7d1354ddf8dfdabcc77eac4e8158b770551
Mohammad-Ali-Rauf <85075073+Mohammad-Ali-Rauf@users.noreply.github.com>
2022-02-21 16:29:08 +0500

    -
    -

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-// const cors = require('cors');
+const cors = require('cors');
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ extended : false }));
 
 connectDB();

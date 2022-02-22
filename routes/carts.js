033cb8275fef8ca9d5dcf0256dcfb660a9638b21
Mohammad-Ali-Rauf <85075073+Mohammad-Ali-Rauf@users.noreply.github.com>
2022-02-22 12:01:22 +0500

    -
    -

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const Cart = require("../models/Cart");
 const Product = require("../models/Product");
+const Order = require("../models/Order");
 const auth = require("../middleware/auth");
 
 // @route GET /api/carts
@@ -20,14 +21,14 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-// @route PUT /api/products/:id
-// @desc  Update A Product
+// @route PUT /api/carts/:productID
+// @desc  Update Cart Product
 // @access Private
 router.put(
-    "/:id",
-    [auth, [
-      check("products", "Please Enter a vaild data").isArray(),
-    ]
+  "/update/:productID",
+  [
+    auth,
+    [check("selectedQuantity", "Please Enter a vaild Quantity.").isNumeric()],
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -35,36 +36,58 @@ router.put(
       return res.status(400).json({ errors: errors.array() });
 
     try {
-      let product = await Product.findById(req.params.id.trim());
+      let cart = await Cart.findById(req.user.id);
 
-      if (!product) {
-        return res.status(400).json({ msg: "Product does not exist." });
+      if (!cart) {
+        return res.status(400).json({ msg: "Cart does not exist." });
       }
 
-      if (product.ownerID.toString() !== req.user.id.trim()) {
-        return res
-          .status(401)
-          .json({ msg: "User have not access to update this product." });
+      let productFound = false;
+
+      cart.products.forEach((product) => {
+        if (product.id === req.params.id) productFound = true;
+      });
+
+      if (!productFound) {
+        res.status(400).json({ msg: "Product does not exist in your cart." });
       }
 
-      const { title, quantity, price, category, description } = req.body;
+      cart.products = cart.products.map(async (product) => {
+        if (product.id === req.params.productID) {
+          try {
+            let sellerProduct = await Product.findById(req.params.productID);
+
+            if (!sellerProduct) {
+              res
+                .status(400)
+                .json({ msg: "Product does not exist on Seller end." });
+            }
+
+            if (req.body.selectedQuantity > sellerProduct.quantity) {
+              res.status(400).json({ msg: "Out of Stock" });
+            }
+
+            product.selectedQuantity = req.body.selectedQuantity;
+          } catch (err) {
+            console.log("Error", err);
+            res.status(500).json({ msg: "Server Error" });
+          }
+        }
+        return product;
+      });
 
       const changes = {
-        title,
-        quantity,
-        price,
-        category,
-        description,
+        products: [...cart.products],
       };
 
-      product = await Product.findByIdAndUpdate(
-        req.params.id.trim(),
+      cart = await Cart.findByIdAndUpdate(
+        cart.id.trim(),
         { $set: changes },
         { new: true }
       );
 
       res.status(200).json({
-        product,
+        cart,
       });
     } catch (err) {
       console.log("Error ", err);
@@ -73,27 +96,106 @@ router.put(
   }
 );
 
-// @route DELETE /api/products/:id
-// @desc  Delete A Product
+// @route DELETE /api/carts/:productID
+// @desc  Delete Cart Product
 // @access Private
-router.delete("/:id", auth, async (req, res) => {
+router.delete("/:productID", auth, async (req, res) => {
   try {
-    let product = await Product.findById(req.params.id.trim());
+    let cart = await Cart.findById(req.user.id);
 
-    if (!product) {
-      return res.status(400).json({ msg: "Product does not exist." });
+    if (!cart) {
+      return res.status(400).json({ msg: "Cart does not exist." });
     }
 
-    if (product.ownerID.toString() !== req.user.id.trim()) {
-      return res
-        .status(401)
-        .json({ msg: "User have not access to update this product." });
+    let productFound = false;
+
+    cart.products.forEach((product) => {
+      if (product.id === req.params.id) productFound = true;
+    });
+
+    if (!productFound) {
+      res.status(400).json({ msg: "Product does not exist in your cart." });
     }
 
-    product = await Product.findByIdAndRemove(req.params.id.trim());
+    cart.products = cart.products.filter(
+      (product) => product.productID !== req.params.productID
+    );
+
+    const changes = {
+      products: [...cart.products],
+    };
+
+    cart = await Cart.findByIdAndUpdate(
+      cart.id.trim(),
+      { $set: changes },
+      { new: true }
+    );
 
     res.status(200).json({
-      msg: "The Product has been deleted",
+      cart,
+    });
+  } catch (err) {
+    console.log("Error ", err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route DELETE /api/carts/buy
+// @desc  Buy All Cart Products
+// @access Private
+router.delete("/buy", auth, async (req, res) => {
+  try {
+    let cart = await Cart.findById(req.user.id);
+
+    if (!cart) {
+      return res.status(400).json({ msg: "Cart does not exist." });
+    }
+
+    cart.products.map(async (product) => {
+      let sellerProduct = await Product.findById(product.productID);
+      if (product.selectedQuantity > sellerProduct.quantity) {
+        return res
+          .status(400)
+          .json({
+            msg: `Product ${sellerProduct.title} has ${sellerProduct.quantity}units available only.`,
+          });
+      }
+    });
+
+    cart.products.map(async product => {
+      let sellerProduct = await Product.findById(product.productID);
+      await Product.findByIdAndUpdate(product.productID, {
+        $set: {
+          quantity: sellerProduct.quantity - product.selectedQuantity
+        }
+      }, { new: true });
+      let orders = await Order.findOne({ ownerID: req.user.id })
+      await Order.findByIdAndUpdate(orders.id, {
+        $set: {
+          products: [
+            ...orders.products,
+            {
+              productID: product.productID,
+              buyQuantity: product.selectedQuantity,
+              buyerID: req.user.id,
+            }
+          ]
+        }
+      }, { new: true });
+    });
+
+    const changes = {
+      products: [],
+    };
+
+    cart = await Cart.findByIdAndUpdate(
+      cart.id.trim(),
+      { $set: changes },
+      { new: true }
+    );
+
+    res.status(200).json({
+      cart,
     });
   } catch (err) {
     console.log("Error ", err);
